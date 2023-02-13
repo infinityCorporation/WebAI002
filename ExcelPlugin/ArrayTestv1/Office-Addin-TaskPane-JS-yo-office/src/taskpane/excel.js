59bc61a2a61376e3f60d0fb7e9cd796a8147151c
Evantf1 <efrisbie2232@gmail.com>
2023-02-12 21:59:53 -0500

    try again

--- ExcelPlugin/ArrayTestv1/Office-Addin-TaskPane-JS-yo-office/src/taskpane/excel.js
+++ ExcelPlugin/ArrayTestv1/Office-Addin-TaskPane-JS-yo-office/src/taskpane/excel.js
@@ -1,12 +1,7 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
-/* global console, document, Excel, Office */
-
 Office.onReady((info) => {
   if (info.host === Office.HostType.Excel) {
+    document.getElementById("intro");
+    document.getElementById("outro");
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
     document.getElementById("run").onclick = run;
