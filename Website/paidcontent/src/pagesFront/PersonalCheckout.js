f298955edf05081ebe694ee8ccbc7d36442a2e79
Evantf1 <efrisbie2232@gmail.com>
2023-02-28 01:42:34 -0500

    stripe.js module and script added, not sure which will be used, saving both

--- Website/paidcontent/src/pagesFront/PersonalCheckout.js
+++ Website/paidcontent/src/pagesFront/PersonalCheckout.js
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import './PersonalCheckout.css';
 
+//Need stripe.js for this page
+
 export default function PersonalCheckout() {
     return(
         <div id="personalCheckoutDiv">
             <h3>
                 This is the checkout page for the personal account.
             </h3>
+            <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet"></link>
         </div>
     )
 }
